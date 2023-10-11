92fa99de5a3805f82a2be65946fcbfb2aa528d9b
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-10-10 23:10:21 -0700

    upd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.NODE_ENV==='production' ? '/ArtfulSpaces' : '/'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
